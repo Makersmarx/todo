2a4327f0b96c5934c9395f7d5fe39304290ac1e9
Makersmarx <bbiehn1@gmail.com>
2020-10-14 11:49:34 -0600

    todo app style changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './styles.css';
 function App() {
   return (
     <div className="todo-list">
+      <h1>Lets Get Organized</h1>
       <TodoItem />
       <TodoItem />
       <TodoItem />
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ function TodoItem() {
   return (
     <div className="todo-item">
       <input type="checkbox" />
-      <p>Placeholder text here</p>
+      <p>Todo: Placeholder</p>
     </div>
   );
 }
