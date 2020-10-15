da1c2c01f6e4ec2335b48de01ce37712765cb24a
Makersmarx <bbiehn1@gmail.com>
2020-10-15 17:42:52 -0600

    more todo practice

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import TodoItem from './components/TodoItem';
 import './styles.css';
 import todosData from './todosData';
 
-function App() {
-  const list = todosData.map((item) => {
-    return <TodoItem key={item.id} item={item} />;
-  });
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+  }
 
-  return (
-    <div className="todo-list">
-      <h1>Lets Get Organized</h1>
-      {list}
-    </div>
-  );
+  render() {
+    const list = this.state.todos.map((item) => {
+      return <TodoItem key={item.id} item={item} />;
+    });
+
+    return (
+      <div className="todo-list">
+        <h1>Lets Get Organized</h1>
+        {list}
+      </div>
+    );
+  }
 }
 
 export default App;
