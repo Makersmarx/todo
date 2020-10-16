3ce16603e6c8d80289ba18ede4f8b68b04fffd3f
Makersmarx <bbiehn1@gmail.com>
2020-10-16 07:36:36 -0600

    todo updates

--- src/App.js
+++ src/App.js
@@ -9,19 +9,32 @@ class App extends React.Component {
     this.state = {
       todos: todosData,
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos,
+      };
+    });
   }
 
   render() {
-    const list = this.state.todos.map((item) => {
-      return <TodoItem key={item.id} item={item} />;
-    });
+    const todoItems = this.state.todos.map((item) => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
 
-    return (
-      <div className="todo-list">
-        <h1>Lets Get Organized</h1>
-        {list}
-      </div>
-    );
+    return <div className="todo-list">{todoItems}</div>;
   }
 }
 
